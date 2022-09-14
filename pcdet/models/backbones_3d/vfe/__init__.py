6c0cacd0cc5b36217eda038225c779e971700760
“agent-sgs” <sgshit@163.com>
2022-09-14 14:33:28 +0800

    pillarnet

--- pcdet/models/backbones_3d/vfe/__init__.py
+++ pcdet/models/backbones_3d/vfe/__init__.py
@@ -2,8 +2,8 @@ from .mean_vfe import MeanVFE
 from .pillar_vfe import PillarVFE
 from .dynamic_mean_vfe import DynamicMeanVFE
 from .dynamic_filter_mean_vfe import DynamicFilterMeanVFE
-from .dynamic_pillar_vfe import DynamicPillarVFE
 from .dynamic_filter_pillar_vfe import DynamicFilterPillarVFE
+from .dynamic_pillar_vfe import DynamicPillarVFE, DynamicPillarPFE
 from .image_vfe import ImageVFE
 from .vfe_template import VFETemplate
 
@@ -14,7 +14,7 @@ __all__ = {
     'ImageVFE': ImageVFE,
     'DynMeanVFE': DynamicMeanVFE,
     'DynFilterMeanVFE': DynamicFilterMeanVFE,
-    'DynFilterMeanVFE': DynamicFilterMeanVFE,
     'DynPillarVFE': DynamicPillarVFE,
     'DynFilterPillarVFE': DynamicFilterPillarVFE,
+    'DynamicPillarPFE': DynamicPillarPFE
 }
