be72ad71ba91cc9d361fda7bf454a78d3d3e9009
ahmedius2 <ahmedius2@gmail.com>
2023-09-26 01:03:44 +0000

    Allow projection coeff to be set from env

--- pcdet/models/detectors/anytime_template_v2.py
+++ pcdet/models/detectors/anytime_template_v2.py
@@ -10,6 +10,7 @@ import numpy as np
 import scipy
 import gc
 import copy
+import os
 
 from ...ops.cuda_projection import cuda_projection
 from ...ops.cuda_point_tile_mask import cuda_point_tile_mask
@@ -112,6 +113,9 @@ class AnytimeTemplateV2(Detector3DTemplate):
 
         self.pc_range = self.vfe.point_cloud_range.cpu()
         self.projection_stream = torch.cuda.Stream()
+
+        self.projection_coeff = float(os.getenv("PROJECTION_COEFF", 1.0))
+        print('Projection coefficient is 1.0')
 
     def initialize(self, batch_dict):
         batch_dict['projections_nms'] = None
@@ -367,9 +371,8 @@ class AnytimeTemplateV2(Detector3DTemplate):
 
             # Remove detections which are no more needed
             active_num_dets = torch.sum(self.num_dets_per_tile)
-            #NOTE we need to tune the coeff here , 2.0 is giving good results!
-            coeff = 2.0
-            max_num_proj = int(active_num_dets * coeff)
+            #NOTE we need to tune the coeff here , 2.0 is giving good results! not for 150 ms...
+            max_num_proj = int(active_num_dets * self.projection_coeff)
             if self.past_detections['pred_boxes'].size(0) > max_num_proj:
                 # Remove oldest dets
                 for k in ['pose_idx', 'pred_boxes', 'pred_labels']:
