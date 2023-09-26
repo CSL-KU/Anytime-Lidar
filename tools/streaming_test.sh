4cc1c18a4bf5a7d593d8d39ad305e4830561800e
ahmedius2 <ahmedius2@gmail.com>
2023-09-26 01:08:10 +0000

    streaming test script updated

--- tools/streaming_test.sh
+++ tools/streaming_test.sh
@@ -3,36 +3,53 @@
 mkdir -p $1
 
 # Baseline
-for E2E_REL_DEADLINE_S in $(seq 0.200 0.050 0.500)
-do
-	export E2E_REL_DEADLINE_S=$E2E_REL_DEADLINE_S
-	# no budget
-	fpath=$1/s${E2E_REL_DEADLINE_S}_eval_dict_m0_d10.0000.json
-	if [ -f $fpath ]; then
-		printf "Skipping $fpath test.\n"
-	else
-		./run_tests.sh singlems 0 10.0000
-		mv -f eval_dict_*.json $fpath
-	fi
-done
-
-# ALv2-ARR
-#MAX_BUDGET=0.240
-#for E2E_REL_DEADLINE_S in $(seq 0.200 0.050 0.500)
+#for E2E_REL_DEADLINE_S in $(seq 0.200 0.100 0.500)
 #do
 #	export E2E_REL_DEADLINE_S=$E2E_REL_DEADLINE_S
-#	for BUDGET in $(seq 0.180 0.020 $MAX_BUDGET)
-#	do
-#		if (( $(echo "$E2E_REL_DEADLINE_S < $BUDGET" |bc -l) )); then
-#			break
-#		fi
-#		fpath=$1/s${E2E_REL_DEADLINE_S}_eval_dict_m4_d${BUDGET}0.json
-#		if [ -f $fpath ]; then
-#			printf "Skipping $fpath test.\n"
-#		else
-#			printf "Doing $fpath test.\n"
-#			./run_tests.sh singlems 4 ${BUDGET}0
-#			mv -f eval_dict_*.json $fpath
-#		fi
-#	done
+#	# no budget
+#	fpath=$1/s${E2E_REL_DEADLINE_S}_eval_dict_m0_d10.0000.json
+#	if [ -f $fpath ]; then
+#		printf "Skipping $fpath test.\n"
+#	else
+#		./run_tests.sh singlems 0 10.0000
+#		mv -f eval_dict_*.json $fpath
+#		fpath=$(echo $fpath | sed 's/json/pkl/g')
+#		mv -f 'eval.pkl' $fpath
+#	fi
 #done
+
+## ALv2-ARR
+. nusc_sh_utils.sh
+link_data 50
+export CALIBRATION=0
+export DO_EVAL=0
+export PROJECTION_COEFF=1.0
+MAX_BUDGET=0.250
+for method in 3
+do
+#for PROJ_COEFF in $(seq 1.0 0.5 2.0)
+#do
+#	TARGET_DIR=$1_$PROJ_COEFF
+	TARGET_DIR=$1
+	for E2E_REL_DEADLINE_S in $(seq 0.200 0.100 0.500)
+	do
+		export E2E_REL_DEADLINE_S=$E2E_REL_DEADLINE_S
+		for BUDGET in $(seq 0.150 0.050 $MAX_BUDGET)
+		do
+			if (( $(echo "$E2E_REL_DEADLINE_S < $BUDGET" | bc -l) )); then
+				break
+			fi
+			fpath=$TARGET_DIR/s${E2E_REL_DEADLINE_S}_eval_dict_m${method}_d${BUDGET}0.json
+			if [ -f $fpath ]; then
+				printf "Skipping $fpath test.\n"
+			else
+				printf "Doing $fpath test.\n"
+				./run_tests.sh singlems $method ${BUDGET}0
+				mv -f eval_dict_*.json $fpath
+				fpath=$(echo $fpath | sed 's/json/pkl/g')
+				mv -f 'eval.pkl' $fpath
+			fi
+		done
+	done
+#done
+done
