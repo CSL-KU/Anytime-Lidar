d82b43dbbbb0fa58cf2a5765ea51aeb40386de70
ahmedius2 <ahmedius2@gmail.com>
2023-09-26 16:12:15 +0000

    updated streaming eval plot

--- tools/get_total_cpu_time.py
+++ tools/get_total_cpu_time.py
@@ -7,12 +7,12 @@ import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
-ALv2_res_path = './streaming_eval_ALv2'
-BL_res_path = './streaming_eval_baseline'
+ALv2_res_path = '/root/shared_data/streaming_eval_ALv2_method2'
+BL_res_path = '/root/shared_data/streaming_eval_baseline'
 
 def get_data_dict(results_path, budgets_str, e2e_deadlines_str):
     data_dict = {e2e_dl:{b:list() for b in budgets_str} for e2e_dl in e2e_deadlines_str}
-    paths = glob.glob(results_path + '/*')
+    paths = glob.glob(results_path + '/*.json')
     for path in paths:
         with open(path, 'r') as handle:
             eval_dict= json.load(handle)
@@ -25,20 +25,24 @@ def get_data_dict(results_path, budgets_str, e2e_deadlines_str):
                 e2e_times_ms = eval_dict['exec_times']['End-to-end'] 
                 total_e2e_time_ms = sum(e2e_times_ms)
                 #utilization = total_e2e_time_ms / (1961 * 50.)
-                utilization = total_e2e_time_ms / (2000 * 50.)
+                utilization = total_e2e_time_ms / (7385 * 50. + 19 * 300)
 
                 data_dict[e2e_dl][budget].extend([mAP, NDS, utilization])
     return data_dict
 
-max_mAP=0.65
-budgets = np.arange(0.180, 0.241, 0.020)
+eval_type="mAP"
+#eval_type="NDS"
+eval_map = {"mAP": [0, 0.55235], "NDS": [1, 0.6311]}
+budgets = np.arange(0.150, 0.251, 0.050)
 e2e_deadlines = np.arange(0.200, 0.501, 0.100)
 budgets_str = [str(round(b,3)) for b in budgets]
 e2e_deadlines_str = [str(round(e2e_dl,3)) for e2e_dl in e2e_deadlines]
 
 fig, axes = plt.subplots(1, 2, figsize=(12, 6), constrained_layout=True)
-for ax, idx, ylabel, div, ylim in zip (axes, (0, 2), ('Normalized accuracy(%)', 'Utilization(%)'), \
-        (max_mAP*0.01, 0.01), ((0., 50.),(0.,100.))):
+for ax, idx, ylabel, div, ylim in zip (axes, (eval_map[eval_type][0], 2), \
+        (f'Normalized {eval_type}(%)', 'Utilization(%)'), \
+        (eval_map[eval_type][1]*0.01, 0.01), \
+        ((0., 75.),(0.,100.))):
     data_dict = get_data_dict(ALv2_res_path, budgets_str, e2e_deadlines_str)
     for budget in budgets_str:
         mask = (e2e_deadlines >= float(budget)-0.001)
@@ -62,4 +66,4 @@ for ax, idx, ylabel, div, ylim in zip (axes, (0, 2), ('Normalized accuracy(%)', 
     ax.set_xlabel('Period (sec)', fontsize='x-large')
     ax.legend(ncol=2)
     ax.set_ylim(ylim)
-plt.savefig("/root/shared_data/stream_eval.png")
+plt.savefig(f"/root/shared_data/stream_eval_{eval_type}.png")
