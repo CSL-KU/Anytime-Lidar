0275aecbcdb7802d880b4827a682647d0f7a55dc
ahmedius2 <ahmedius2@gmail.com>
2023-09-27 19:49:15 +0000

    Amended batch test script

--- tools/calib_and_run_tests.sh
+++ tools/calib_and_run_tests.sh
@@ -2,34 +2,24 @@
 . nusc_sh_utils.sh
 
 ## CALIBRATION
-#export CALIBRATION=1
-#link_data 250
+#link_data 500
 #./run_tests.sh calibm 2
 
 ## TEST
-export CALIBRATION=0
-export DO_EVAL=0
-#link_data 500
-#./run_tests.sh methods 0.500 -0.050 0.451
-#link_data 450
-#./run_tests.sh methods 0.450 -0.050 0.401
-#link_data 400
-#./run_tests.sh methods 0.400 -0.050 0.351
-#link_data 350
-#./run_tests.sh methods 0.350 -0.050 0.301
-link_data 300
-./run_tests.sh methods 0.300 -0.050 0.251
-link_data 300
-./run_tests.sh methods 0.300 -0.050 0.251
-link_data 250
-./run_tests.sh methods 0.250 -0.050 0.201
-link_data 200
-./run_tests.sh methods 0.200 -0.050 0.151
-link_data 150
-./run_tests.sh methods 0.150 -0.050 0.101
-link_data 100
-./run_tests.sh methods 0.100 -0.050 0.051
-link_data 50
-./run_tests.sh methods 0.050 -0.050 0.001
-python eval_from_files.py ./exp_data_nsc
+#AGX Orin
+start_period_ms=300
+end_period_ms=50
+#AGX Xavier
+#start_period_ms=500
+#end_period_ms=200
+
+step_ms=50 # better not change it
+step_s=$(echo "$step_ms / 1000.0" | bc -l)
+for period_ms in $(seq $start_period_ms -$step_ms $end_period_ms); do
+	link_data $period_ms
+	period_s=$(echo "$period_ms / 1000.0" | bc -l)
+	./run_tests.sh methods $(printf %.3f $period_s) -0.050 \
+		$(printf %.3f $(echo "$period_s - $step_s + 0.001" | bc -l))
+	python eval_from_files.py ./exp_data_nsc
+done
 #. streaming_test.sh streaming_eval_ALv2
