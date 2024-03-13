33a114d8e1c9dcafb4c7e7768d67325a422a9f39
ahmedius2 <ahmedius2@gmail.com>
2024-03-13 18:41:52 +0000

    updated log plotter

--- tools/log_plotter.py
+++ tools/log_plotter.py
@@ -26,38 +26,22 @@ method_colors= [
     'tab:purple', 
 ]
 
-##HACK
-#method_num_to_str = [
-#        '0VALO-CP75-500ms',
-#        '1VALO-CP75-350ms',
-#        '2VALO-CP75-250ms',
-#        '3VALO-CP75-150ms',
-#]
-#method_remap = {
-#        0:1, 1:2, 2:3, 3:0
-#}
-
 method_num_to_str = [
         '0CenterPoint75',
-        '1AnytimeLidar-CP75',
-        '2VALO-CP75',
-        #'3VALO-ARR',
-        '0',
-        '4VALO-NF-CP75',
-        '5CenterPoint100',
-        '6CenterPoint200',
-        '7VALO-NSNF-CP75',
-        '8VoxelNext75',
-        '9VALO-VN75',
-        '0',
-        '1VALO-CP100',
-        '2VALO-CP75-BLTP',
-        '3VALO-VN75-BLTP',
+        '1CenterPoint100',
+        '2CenterPoint200',
+        '3VoxelNext75',
+        '4VALO-CP75',
+        '5VALO-BLTP-CP75',
+        '6VALO-VN75',
+        '7VALO-BLTP-VN75',
+        '8VALO-NF-CP75',
+        '9VALO-NSNF-CP75',
+        '9AnytimeLidar-CP75',
+        '9VALO-CP100',
 ]
 
-method_remap = {
-        0:0, 2:2, 10:1, 3:10, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 11:3, 12:12, 13:13
-}
+method_remap = {i:i for i in range(method_num_to_str)}
 
 if __name__ == '__main__':
     inp_dir = sys.argv[1]
@@ -90,17 +74,8 @@ if __name__ == '__main__':
     exp_names = sorted(exps_dict.keys())
     exps_dict= {nm[1:]:exps_dict[nm] for nm in exp_names}
 
-    # Filter some
-    #exps_dict = {nm:exps_dict[nm] for nm in ['Baseline-3', 'Baseline-2', 'Baseline-1', 'Impr-MS-HS-A-P']}
-
     plot_sets=[]
 
-#    plot_sets.append({ nm:exps_dict[nm] for nm in [ \
-#        'VALO-CP75-150ms',
-##        'VALO-CP75-250ms',
-#        'VALO-CP75-350ms',
-#        'VALO-CP75-500ms',
-#    ]})
 
     plot_sets.append({ nm:exps_dict[nm] for nm in [ \
             'CenterPoint75',
@@ -119,21 +94,15 @@ if __name__ == '__main__':
             'CenterPoint75',
             'VALO-NSNF-CP75',
             'VALO-NF-CP75',
+            'VALO-BLTP-CP75',
             'VALO-CP75',
     ]})
 
     plot_sets.append({ nm:exps_dict[nm] for nm in [ \
             'VoxelNext75',
+            'VALO-BLTP-VN75',
             'VALO-VN75',
-            'VALO-VN75-BLTP',
     ]})
-
-#    plot_sets.append({ nm:exps_dict[nm] for nm in [ \
-#            'CenterPoint-75',
-#            'CenterPoint-100',
-#            'CenterPoint-PP',
-#            'VALO',
-#    ]})
 
     plot_set_choice = int(sys.argv[2])
     exps_dict=plot_sets[plot_set_choice]
