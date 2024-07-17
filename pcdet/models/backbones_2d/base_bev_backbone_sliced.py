2539a750fb4b298d5eacf972c3a53a44ff1bd98f
ahmedius2 <ahmedius2@gmail.com>
2024-07-17 01:36:11 +0000

    Organized the backbone code

--- pcdet/models/backbones_2d/base_bev_backbone_sliced.py
+++ pcdet/models/backbones_2d/base_bev_backbone_sliced.py
@@ -6,9 +6,89 @@ import time
 from ..detectors.sched_helpers import SchedAlgo
 from .base_bev_backbone import BasicBlock
 
-class BaseBEVBackboneSliced(nn.Module):
-    def __init__(self, model_cfg, input_channels):
+class BaseBEVBackboneSlicedBase(nn.Module):
+    def __init__(self, model_cfg):
         super().__init__()
+        self.tcount = model_cfg.TILE_COUNT
+        self.sched_algo = model_cfg.METHOD
+
+    def forward(self, pruned_spatial_features):
+        x = pruned_spatial_features
+        ups = []
+        ret_dict = {}
+        for i in range(len(self.blocks)):
+            x = self.blocks[i](x)
+
+            #stride = int(spatial_features.shape[2] / x.shape[2])
+            #ret_dict['spatial_features_%dx' % stride] = x
+
+            if len(self.deblocks) > 0:
+                ups.append(self.deblocks[i](x))
+            else:
+                ups.append(x)
+
+        if len(ups) > 1:
+            x = torch.cat(ups, dim=1)
+        elif len(ups) == 1:
+            x = ups[0]
+
+        if len(self.deblocks) > len(self.blocks):
+            x = self.deblocks[-1](x)
+
+        #NOTE returned x should be assigned as following before forwarding it to dethead
+        #data_dict['spatial_features_2d'] = x
+        return x
+
+    def prune_spatial_features(self,data_dict):
+        spatial_features = data_dict['spatial_features']
+
+        ctc = data_dict['chosen_tile_coords']
+        if self.sched_algo == SchedAlgo.MirrorRR:
+            ctc = np.sort(ctc)
+        ctc_s, ctc_e = ctc[0], ctc[-1]
+        tile_sz = spatial_features.size(-1) // self.tcount
+        if len(ctc) == self.tcount:
+            # Select all
+            x = spatial_features
+            chunks = [(ctc_s, ctc_e)]
+        elif (self.sched_algo == SchedAlgo.RoundRobin and ctc_s <= ctc_e) or \
+                 (self.sched_algo == SchedAlgo.AdaptiveRR and ctc_s <= ctc_e) or \
+                 (self.sched_algo == SchedAlgo.MirrorRR and ctc_e - ctc_s + 1 == ctc.shape[0]):
+            # Contiguous
+            x = spatial_features[..., (ctc_s * tile_sz):((ctc_e + 1) * tile_sz)]
+            chunks = [(ctc_s, ctc_e)]
+        else:
+            # Two chunks, find the point of switching
+            # Following piece of code take 0.6 ms in jetson agx
+            i = 0
+            if self.sched_algo == SchedAlgo.RoundRobin or self.sched_algo == SchedAlgo.AdaptiveRR:
+                while ctc[i] < ctc[i+1]:
+                    i += 1
+            elif self.sched_algo == SchedAlgo.MirrorRR:
+                while ctc[i]+1 == ctc[i+1]:
+                    i += 1
+            chunk_r = (ctc_s, ctc[i])
+            chunk_l = (ctc[i+1], ctc_e)
+            c_sz_l = (chunk_l[1] - chunk_l[0] + 1) * tile_sz
+            c_sz_r = (chunk_r[1] - chunk_r[0] + 1) * tile_sz
+            c_sz = list(spatial_features.size())
+            c_sz[-1] = c_sz_r + c_sz_l
+            x = torch.empty(c_sz, device='cuda', dtype=spatial_features.dtype)
+            #Example: . . 2 3 4 . . 7 8 -> 7 8 2 3 4
+            x[..., :c_sz_r] = spatial_features[..., \
+                    (chunk_r[0]*tile_sz):((chunk_r[1]+1)*tile_sz)]
+            x[..., -c_sz_l:] = spatial_features[..., \
+                    (chunk_l[0]*tile_sz):((chunk_l[1]+1)*tile_sz)]
+            chunks = [chunk_r, chunk_l]
+        data_dict['tile_chunks'] = chunks
+        data_dict['num_tiles_in_sf'] = x.size(3)//tile_sz
+        data_dict['spatial_features'] = x.contiguous()
+
+        return data_dict
+
+class BaseBEVBackboneSliced(BaseBEVBackboneSlicedBase):
+    def __init__(self, model_cfg, input_channels):
+        super().__init__(model_cfg)
         self.model_cfg = model_cfg
 
         if self.model_cfg.get('LAYER_NUMS', None) is not None:
@@ -60,7 +140,7 @@ class BaseBEVBackboneSliced(nn.Module):
                         nn.ReLU()
                     ))
                 else:
-                    stride = np.round(1 / stride).astype(np.int)
+                    stride = np.round(1 / stride).astype(np.int32)
                     self.deblocks.append(nn.Sequential(
                         nn.Conv2d(
                             num_filters[idx], num_upsample_filters[idx],
@@ -80,91 +160,11 @@ class BaseBEVBackboneSliced(nn.Module):
             ))
 
         self.num_bev_features = c_in
-        self.tcount = self.model_cfg.TILE_COUNT
-        self.sched_algo = self.model_cfg.METHOD
-
-    def forward(self, data_dict):
-        """
-        Args:
-            data_dict:
-                spatial_features
-        Returns:
-        """
-
-        spatial_features = data_dict['spatial_features']
-
-        ctc = data_dict['chosen_tile_coords']
-        if self.sched_algo == SchedAlgo.MirrorRR:
-            ctc = np.sort(ctc)
-        ctc_s, ctc_e = ctc[0], ctc[-1]
-        tile_sz = spatial_features.size(-1) // self.tcount
-        if len(ctc) == self.tcount:
-            # Select all
-            x = spatial_features
-            chunks = [(ctc_s, ctc_e)]
-        elif (self.sched_algo == SchedAlgo.RoundRobin and ctc_s <= ctc_e) or \
-                 (self.sched_algo == SchedAlgo.AdaptiveRR and ctc_s <= ctc_e) or \
-                 (self.sched_algo == SchedAlgo.MirrorRR and ctc_e - ctc_s + 1 == ctc.shape[0]):
-            # Contiguous
-            x = spatial_features[..., (ctc_s * tile_sz):((ctc_e + 1) * tile_sz)]
-            chunks = [(ctc_s, ctc_e)]
-        else:
-            # Two chunks, find the point of switching
-            # Following piece of code take 0.6 ms in jetson agx
-            i = 0
-            if self.sched_algo == SchedAlgo.RoundRobin or self.sched_algo == SchedAlgo.AdaptiveRR:
-                while ctc[i] < ctc[i+1]:
-                    i += 1
-            elif self.sched_algo == SchedAlgo.MirrorRR:
-                while ctc[i]+1 == ctc[i+1]:
-                    i += 1
-            chunk_r = (ctc_s, ctc[i])
-            chunk_l = (ctc[i+1], ctc_e)
-            c_sz_l = (chunk_l[1] - chunk_l[0] + 1) * tile_sz
-            c_sz_r = (chunk_r[1] - chunk_r[0] + 1) * tile_sz
-            c_sz = list(spatial_features.size())
-            c_sz[-1] = c_sz_r + c_sz_l
-            x = torch.empty(c_sz, device='cuda', dtype=spatial_features.dtype)
-            #Example: . . 2 3 4 . . 7 8 -> 7 8 2 3 4
-            x[..., :c_sz_r] = spatial_features[..., \
-                    (chunk_r[0]*tile_sz):((chunk_r[1]+1)*tile_sz)]
-            x[..., -c_sz_l:] = spatial_features[..., \
-                    (chunk_l[0]*tile_sz):((chunk_l[1]+1)*tile_sz)]
-            chunks = [chunk_r, chunk_l]
-        data_dict['tile_chunks'] = chunks
-
-        ups = []
-        ret_dict = {}
-        for i in range(len(self.blocks)):
-            x = self.blocks[i](x)
-
-            #stride = int(spatial_features.shape[2] / x.shape[2])
-            #ret_dict['spatial_features_%dx' % stride] = x
-            if len(self.deblocks) > 0:
-                ups.append(self.deblocks[i](x))
-            else:
-                ups.append(x)
-
-        if len(ups) > 1:
-            x = torch.cat(ups, dim=1)
-        elif len(ups) == 1:
-            x = ups[0]
-
-        if len(self.deblocks) > len(self.blocks):
-            x = self.deblocks[-1](x)
-
-        data_dict['spatial_features_2d'] = x
-
-        # Don't scatter the results now, let detection head process the reduced
-        # input as well.
-
-        return data_dict
 
 
-
-class BaseBEVResBackboneSliced(nn.Module):
+class BaseBEVResBackboneSliced(BaseBEVBackboneSlicedBase):
     def __init__(self, model_cfg, input_channels):
-        super().__init__()
+        super().__init__(model_cfg)
         self.model_cfg = model_cfg
 
         if self.model_cfg.get('LAYER_NUMS', None) is not None:
@@ -209,7 +209,7 @@ class BaseBEVResBackboneSliced(nn.Module):
                         nn.ReLU()
                     ))
                 else:
-                    stride = np.round(1 / stride).astype(np.int)
+                    stride = np.round(1 / stride).astype(np.int32)
                     self.deblocks.append(nn.Sequential(
                         nn.Conv2d(
                             num_filters[idx], num_upsample_filters[idx],
@@ -229,80 +229,4 @@ class BaseBEVResBackboneSliced(nn.Module):
             ))
 
         self.num_bev_features = c_in
-        self.tcount = self.model_cfg.TILE_COUNT
-        self.sched_algo = self.model_cfg.METHOD
-
-    def forward(self, data_dict):
-        """
-        Args:
-            data_dict:
-                spatial_features
-        Returns:
-        """
-        spatial_features = data_dict['spatial_features']
-
-        ctc = data_dict['chosen_tile_coords']
-        if self.sched_algo == SchedAlgo.MirrorRR:
-            ctc = np.sort(ctc)
-        ctc_s, ctc_e = ctc[0], ctc[-1]
-        tile_sz = spatial_features.size(-1) // self.tcount
-        if len(ctc) == self.tcount:
-            # Select all
-            x = spatial_features
-            chunks = [(ctc_s, ctc_e)]
-        elif (self.sched_algo == SchedAlgo.RoundRobin and ctc_s <= ctc_e) or \
-                 (self.sched_algo == SchedAlgo.AdaptiveRR and ctc_s <= ctc_e) or \
-                 (self.sched_algo == SchedAlgo.MirrorRR and ctc_e - ctc_s + 1 == ctc.shape[0]):
-            # Contiguous
-            x = spatial_features[..., (ctc_s * tile_sz):((ctc_e + 1) * tile_sz)]
-            chunks = [(ctc_s, ctc_e)]
-        else:
-            # Two chunks, find the point of switching
-            # Following piece of code take 0.6 ms in jetson agx
-            i = 0
-            if self.sched_algo == SchedAlgo.RoundRobin or self.sched_algo == SchedAlgo.AdaptiveRR:
-                while ctc[i] < ctc[i+1]:
-                    i += 1
-            elif self.sched_algo == SchedAlgo.MirrorRR:
-                while ctc[i]+1 == ctc[i+1]:
-                    i += 1
-            chunk_r = (ctc_s, ctc[i])
-            chunk_l = (ctc[i+1], ctc_e)
-            c_sz_l = (chunk_l[1] - chunk_l[0] + 1) * tile_sz
-            c_sz_r = (chunk_r[1] - chunk_r[0] + 1) * tile_sz
-            c_sz = list(spatial_features.size())
-            c_sz[-1] = c_sz_r + c_sz_l
-            x = torch.empty(c_sz, device='cuda', dtype=spatial_features.dtype)
-            #Example: . . 2 3 4 . . 7 8 -> 7 8 2 3 4
-            x[..., :c_sz_r] = spatial_features[..., \
-                    (chunk_r[0]*tile_sz):((chunk_r[1]+1)*tile_sz)]
-            x[..., -c_sz_l:] = spatial_features[..., \
-                    (chunk_l[0]*tile_sz):((chunk_l[1]+1)*tile_sz)]
-            chunks = [chunk_r, chunk_l]
-        data_dict['tile_chunks'] = chunks
-
-        ups = []
-        ret_dict = {}
-        for i in range(len(self.blocks)):
-            x = self.blocks[i](x)
-            
-            #stride = int(spatial_features.shape[2] / x.shape[2])
-            #ret_dict['spatial_features_%dx' % stride] = x
-
-            if len(self.deblocks) > 0:
-                ups.append(self.deblocks[i](x))
-            else:
-                ups.append(x)
-
-        if len(ups) > 1:
-            x = torch.cat(ups, dim=1)
-        elif len(ups) == 1:
-            x = ups[0]
-
-        if len(self.deblocks) > len(self.blocks):
-            x = self.deblocks[-1](x)
-
-        data_dict['spatial_features_2d'] = x
-
-        return data_dict
 
