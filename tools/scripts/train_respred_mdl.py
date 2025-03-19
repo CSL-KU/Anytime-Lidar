4f1375bba6c7bceec27592f2d21c0af8b4ec5041
Ahmet Soyyigit <ahmedius2@gmail.com>
2025-03-19 16:29:04 -0500

    Fixed frame_id bug

--- tools/scripts/train_respred_mdl.py
+++ tools/scripts/train_respred_mdl.py
@@ -14,7 +14,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import pandas as pd
-from visual_utils.bev_visualizer import visualize_bev_detections
+#from visual_utils.bev_visualizer import visualize_bev_detections
 
 #NUMRES = 3
 num_train_scenes = 75
@@ -26,6 +26,22 @@ calc_detscores=False
 NUM_CLASSES = 10
 TIME_SLICE_PER_SCENE = 10 # ~2 seconds
 DISTANCE_THRESHOLDS = [0.5, 1.0, 2.0, 4.0]
+class_names = ['car','truck', 'construction_vehicle', 'bus', 'trailer',
+              'barrier', 'motorcycle', 'bicycle', 'pedestrian', 'traffic_cone']
+class_range = {
+    "car": 50,
+    "truck": 50,
+    "bus": 50,
+    "trailer": 50,
+    "construction_vehicle": 50,
+    "pedestrian": 40,
+    "motorcycle": 40,
+    "bicycle": 40,
+    "traffic_cone": 30,
+    "barrier": 30
+}
+
+class_id_range = [0] + [class_range[name] for name in class_names]
 
 def parse_detresult(result_str):
     result_str = result_str.split('\n')
@@ -59,14 +75,14 @@ def calc_AP(boxes_l, scores_l, gt_boxes_l, dist_th, num_all_dets, num_all_gt):
     scores_sort_inds = np.argsort(merged_scores)[::-1] # reverse it
     tp = np.zeros(num_all_dets, dtype=bool)
     for i, score_i in enumerate(scores_sort_inds):
+        frame_id = merged_frame_ids[score_i]
         gt_boxes = gt_boxes_l_cpy[frame_id]
         if gt_boxes.shape[0] == 0:
             continue
 
         score = merged_scores[score_i]
-        frame_id = merged_frame_ids[score_i]
         box = merged_boxes[score_i]
-        box_xy = np.expand_dims(box[:2], 0)
+        box_xy = np.expand_dims(box, 0)
 
         dists = distance.cdist(gt_boxes[:, :2], box_xy, 'euclidean').flatten()
         min_idx = np.argmin(dists)
@@ -78,29 +94,29 @@ def calc_AP(boxes_l, scores_l, gt_boxes_l, dist_th, num_all_dets, num_all_gt):
     simple_precision = np.sum(tp) / len(tp)
 
     fp = np.logical_not(tp)
-    tp = np.cumsum(tp).astype(float)
-    fp = np.cumsum(fp).astype(float)
+    tp = np.cumsum(tp.astype(float))
+    fp = np.cumsum(fp.astype(float))
 
     prec = tp / (fp + tp)
     rec = tp / float(num_all_gt)
     nelem = 101
     rec_interp = np.linspace(0, 1, nelem)
-    prec = np.interp(rec_interp, rec, prec, right=0)
+    precision = np.interp(rec_interp, rec, prec, right=0)
     #conf = np.interp(rec_interp, rec, scr, right=0)
     rec = rec_interp
 
-    min_recall = 0. # 0.1
-    min_precision = 0. #0.1
+    min_recall =  0.1
+    min_precision = 0.1
 
-    prec = np.copy(prec)
+    prec = np.copy(precision)
     prec = prec[round(100 * min_recall) + 1:]  # Clip low recalls. +1 to exclude the min recall bin.
     prec -= min_precision  # Clip low precision
     prec[prec < 0] = 0
     return float(np.mean(prec)) / (1.0 - min_precision), simple_precision
 
 def calc_detscore(eval_data_arr):
-    detscore = 0.
-    for eval_data_dict in eval_data_arr: #for each class
+    detscore = []
+    for class_idx, eval_data_dict in enumerate(eval_data_arr): #for each class
         boxes_l, scores_l, gt_boxes_l = eval_data_dict['boxes'], \
                 eval_data_dict['scores'], eval_data_dict['gt_boxes']
 
@@ -113,11 +129,11 @@ def calc_detscore(eval_data_arr):
         if num_all_dets != 0:
             for dist_th in DISTANCE_THRESHOLDS:
                 #Append frame id of each score
-                ap_score, _ = calc_AP(boxes_l, scores_l, gt_boxes_l, dist_th,
+                ap_score, simple_precision = calc_AP(boxes_l, scores_l, gt_boxes_l, dist_th,
                                    num_all_dets, num_gt)
-                detscore += ap_score * num_gt
+                detscore.append(ap_score) #* num_gt
 
-    return detscore
+    return sum(detscore) / (len(DISTANCE_THRESHOLDS) * NUM_CLASSES)
 
 def calc_resolution_detscores(sampled_dets, all_tslc_inds, all_gt_boxes, ridx):
     print('Calculating detection scores for resolution', ridx)
@@ -134,11 +150,14 @@ def calc_resolution_detscores(sampled_dets, all_tslc_inds, all_gt_boxes, ridx):
             slc_eval_data = eval_data_arr[cls_id-1]
             boxes_l, scores_l, gt_boxes_l = slc_eval_data['boxes'], \
                     slc_eval_data['scores'], slc_eval_data['gt_boxes']
+            cls_range = class_id_range[cls_id]
             for i in range(si, ei):
                 j = i - si
                 gt_labels = all_gt_boxes[i][:, -1].astype(int)
                 cls_mask = (gt_labels == cls_id)
-                gt_boxes_l[j] = all_gt_boxes[i][cls_mask, :-1].astype(float)
+                cur_gt_boxes = all_gt_boxes[i][cls_mask, :-1]
+                range_mask = (cur_gt_boxes[:, :2] <= cls_range).all(1)
+                gt_boxes_l[j] = cur_gt_boxes[range_mask].astype(float)
 
                 pred_dicts = sampled_dets[i]
                 if pred_dicts is None:
@@ -148,9 +167,14 @@ def calc_resolution_detscores(sampled_dets, all_tslc_inds, all_gt_boxes, ridx):
                     # ind is 0 since batch size assumed to be 1
                     pd = pred_dicts[0]
                     labels = pd['pred_labels']
+                    boxes = pd['pred_boxes']
+                    scores = pd['pred_scores']
                     cls_mask = (labels == cls_id)
-                    boxes_l[j] = pd['pred_boxes'][cls_mask]
-                    scores_l[j] = pd['pred_scores'][cls_mask]
+                    boxes = boxes[cls_mask]
+                    scores = scores[cls_mask]
+                    range_mask = (boxes[:, :2] <= cls_range).all(1)
+                    boxes_l[j] = boxes[range_mask]
+                    scores_l[j] = scores[range_mask]
         tslc_scores[tidx] = calc_detscore(eval_data_arr)
         if tidx % (int(num_timeslices / 10)+1) == 0:
             print(f"Resolution {ridx} detscore calc progress: %{int(tidx/num_timeslices*100)}")
@@ -215,16 +239,30 @@ def read_data(pth):
             all_tslc_scores = json.load(f)
     else:
         all_tslc_scores = np.zeros((len(all_tslc_inds), num_res))
-        with concurrent.futures.ProcessPoolExecutor(max_workers=num_res) as executor:
+        dataset_mAPs = np.zeros(num_res)
+        with concurrent.futures.ProcessPoolExecutor(max_workers=num_res * 2) as executor:
             futs = []
+            noslc_futs = []
             for eval_d in eval_dicts:
                 ridx = eval_d['resolution']
                 fut = executor.submit(calc_resolution_detscores, eval_d['objects'], \
                         all_tslc_inds, all_gt_boxes, ridx)
                 futs.append((ridx, fut))
+
+                noslc_fut = executor.submit(calc_resolution_detscores, eval_d['objects'], \
+                        [[0, len(all_gt_boxes)]], all_gt_boxes, ridx)
+                noslc_futs.append((ridx, noslc_fut))
+
             for ridx, fut in futs:
                 detscores = fut.result()
                 all_tslc_scores[:, ridx] = detscores
+
+            for ridx, fut in noslc_futs:
+                detscores = fut.result()
+                dataset_mAPs[ridx] = detscores[0]
+
+        print('Manually calculated dataset mAPs:')
+        print(dataset_mAPs)
 
         with open(detscore_path, 'w') as f:
             json.dump(all_tslc_scores.tolist(), f, indent=4)
@@ -263,7 +301,7 @@ def read_data(pth):
                 print(idx, scene_egovel_inds[idx], 'No boxes.')
                 continue
 
-            #NOTE the velocities does not help, dunno why
+            # Velocity helps a lot!
             obj_velos = boxes[:, 7:9]
             obj_velos[np.isnan(obj_velos).any(1)] = 0. #Assume these to be stationary
             rel_velos = obj_velos - ev # using ev makes it worse?
@@ -293,11 +331,15 @@ def read_data(pth):
             #   precs[j] = precision * (1 / dist_th)
             #mean_prec = precs.mean()
 
-            # This helps! not all the time tho
+            # This helps!
             objpos = boxes[:, :2]
             objpos = np.linalg.norm(objpos, axis=1)
             objpos_mean = np.mean(objpos)
             objpos_perc5, objpos_perc95 = np.percentile(objpos, (5, 95))
+
+            #labels = pred_dict['pred_labels']
+            #num_obj = labels.shape[0]
+            #label_dist = np.bincount(labels-1, minlength=NUM_CLASSES) / labels.shape[0]
 
             data_tuples.append((objpos_perc5, objpos_mean, objpos_perc95, #))
                 np.linalg.norm(ev),
@@ -312,14 +354,17 @@ def read_data(pth):
 
     # num time_preds is as same as num sampled_dets
     y = np.empty((len(time_preds), num_res))
+    oracle_dict = {}
     for tidx, (si, ei) in enumerate(all_tslc_inds): # for each timeslice
-        num_elem = ei - si
         scores = all_tslc_scores[tidx]
+        for st in sample_tokens[si:ei]:
+            oracle_dict[st] = np.argmax(scores).item()
+        num_elem = ei - si
         scores = np.tile(scores, num_elem).reshape(-1, len(scores))
         y[si:ei] = scores
-    y = y[mask]
+    masked_y = y[mask]
 
-    return X, y
+    return X, masked_y, oracle_dict
 
 def get_bad_data_mask(X, y):
     mask = np.logical_or(np.isnan(X).any(1), np.isnan(y).any(1))
@@ -329,10 +374,13 @@ def get_bad_data_mask(X, y):
 if __name__ == '__main__':
     inp_dir = sys.argv[1]
     if gen_dataset:
-        X_train, y_train = read_data(os.path.join(inp_dir, 'train'))
-        X_test, y_test = read_data(os.path.join(inp_dir, 'test'))
+        X_train, y_train, oracle_train = read_data(os.path.join(inp_dir, 'train'))
+        X_test, y_test, oracle_test = read_data(os.path.join(inp_dir, 'test'))
+        print('Dumping generated dataset and oracle')
         with open(os.path.join(inp_dir, 'generated_dataset.pkl'), 'wb') as f:
             pickle.dump((X_train, y_train, X_test, y_test), f)
+        with open('oracle_test.json', 'w') as f:
+            json.dump(oracle_test, f, indent=4)
     else:
         print('Loading generated dataset')
         with open(os.path.join(inp_dir, 'generated_dataset.pkl'), 'rb') as f:
