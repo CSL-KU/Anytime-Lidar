b55ab6461d2ca4d6798732bbc7d4945348663405
ahmedius2 <ahmedius2@gmail.com>
2025-07-04 19:17:02 +0000

    update slice code

--- tools/do_calib.sh
+++ tools/do_calib.sh
@@ -3,52 +3,39 @@
 
 unset OMP_NUM_THREADS USE_ALV1 TASKSET CFG_FILE CKPT_FILE DEADLINE_RANGE_MS DATASET_RANGE
 export CALIBRATION=1
-#export DEADLINE_RANGE_MS="70-200"
-#export DEADLINE_RANGE_MS="90-350"
-export DATASET_PERIOD="500"
+export DATASET_PERIOD="250"
 #./nusc_dataset_prep.sh
-link_data 500
-NUM_TILES=18
+link_data 250
 
-export CFG_FILE="./cfgs/nuscenes_models/cbgs_dyn_voxel0075_res3d_centerpoint_anytime_18.yaml"
-export CKPT_FILE="../models/cbgs_voxel0075_res3d_centerpoint_anytime_18.pth"
-if ! [ -f calib_data_m4_c"$NUM_TILES".json ]; then
-	./run_tests.sh singlemt 4 100.0 $NUM_TILES
-	rm -f calib_data_m8_c"$NUM_TILES".json calib_data_m9_c"$NUM_TILES".json
-	ln -s calib_data_m4_c"$NUM_TILES".json calib_data_m8_c"$NUM_TILES".json
-	ln -s calib_data_m4_c"$NUM_TILES".json calib_data_m9_c"$NUM_TILES".json
-fi
-
-if ! [ -f calib_data_m5_c"$NUM_TILES".json ]; then
-	./run_tests.sh singlemt 5 100.0 $NUM_TILES
-fi
-
-export CFG_FILE="./cfgs/nuscenes_models/cbgs_dyn_voxel0075_voxelnext_anytime.yaml"
-export CKPT_FILE="../models/voxelnext_nuscenes_kernel1.pth"
-for m in 6 7; do
-	if ! [ -f calib_data_m"$m"_c"$NUM_TILES".json ]; then
-		./run_tests.sh singlemt $m 100.0 $NUM_TILES
-	fi
+############CALIB START################
+export CALIBRATION=1
+CFG_FILE="./cfgs/nuscenes_models/valo_pillarnet_0100.yaml"
+CKPT_FILE="../models/pillarnet0100_e20.pth"
+for m in 5 13
+do
+	CFG_FILE=$CFG_FILE CKPT_FILE=$CKPT_FILE ./run_tests.sh singlem $m 10.0
 done
 
-export CFG_FILE="./cfgs/nuscenes_models/cbgs_dyn_voxel0075_res3d_centerpoint_anytime_v1.yaml"
-export CKPT_FILE="../models/cbgs_voxel0075_res3d_centerpoint_anytime_v1.pth"
-if ! [ -f calib_dict_NuScenesDataset_m10.json ]; then
-	TASKSET="taskset 0x3f" OMP_NUM_THREADS=2 USE_ALV1=1 ./run_tests.sh singlem 10
-fi
+CFG_FILE="./cfgs/nuscenes_models/mural_pillarnet_0100_0128_0200.yaml"
+CKPT_FILE="../models/mural_pillarnet_0100_0128_0200_e20.pth"
+for m in $(seq 6 12)
+do
+	CFG_FILE=$CFG_FILE CKPT_FILE=$CKPT_FILE ./run_tests.sh singlem $m 10.0
+done
 
-# this one doesn't support 18 tiles but 16
-export CFG_FILE="./cfgs/nuscenes_models/cbgs_dyn_voxel01_res3d_centerpoint_anytime_16.yaml"
-export CKPT_FILE="../models/cbgs_voxel01_res3d_centerpoint_anytime_16.pth"
-if ! [ -f calib_data_m11_c16.json ]; then
-	./run_tests.sh singlemt 11 100.0 16
-fi
+CFG_FILE="./cfgs/nuscenes_models/valo_pointpillars_cp_0200.yaml"
+CKPT_FILE="../models/PointPillarsCP0200_e20.pth"
+for m in 5 13
+do
+	CFG_FILE=$CFG_FILE CKPT_FILE=$CKPT_FILE ./run_tests.sh singlem $m 10.0
+done
 
-export CFG_FILE="./cfgs/nuscenes_models/dsvt_anytime.yaml"
-export CKPT_FILE="../models/DSVT_Nuscenes_val.pth"
-if ! [ -f calib_data_m13_c"$NUM_TILES".json ]; then
-	./run_tests.sh singlemt 13 100.0 $NUM_TILES
-fi
+CFG_FILE="./cfgs/nuscenes_models/mural_pp_centerpoint_0200_0256_0400.yaml"
+CKPT_FILE="../models/mural_pp_centerpoint_0200_0256_0400_e20.pth"
+for m in $(seq 6 12)
+do
+	CFG_FILE=$CFG_FILE CKPT_FILE=$CKPT_FILE ./run_tests.sh singlem $m 10.0
+done
+############CALIB END################a
 
-unset CFG_FILE CKPT_FILE NUM_TILES
 printf "Calibration done.\n"
